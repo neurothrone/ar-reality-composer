3f83820d39d0125669e056c47ba64ef4d9a5729e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-16 22:33:08 +0200

    add files to source control

--- ARRealityComposer/ContentView.swift
+++ ARRealityComposer/ContentView.swift
@@ -5,39 +5,78 @@
 //  Created by Zaid Neurothrone on 2022-10-16.
 //
 
-import SwiftUI
 import RealityKit
+import SwiftUI
+
+final class ViewModel: ObservableObject {
+  @Published var text = ""
+  
+  var onScaleCloudNotification: () -> Void = {}
+}
 
 struct ContentView : View {
-    var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+  @StateObject var viewModel: ViewModel = .init()
+  
+  var body: some View {
+    VStack {
+      ARViewContainer(viewModel: viewModel)
+        .edgesIgnoringSafeArea(.all)
+      
+      VStack {
+        Text(viewModel.text)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.horizontal)
+        
+        Button("Scale Cloud") {
+          viewModel.onScaleCloudNotification()
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: 100)
+      .background(.purple)
+      .foregroundColor(.white)
     }
+  }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+  let viewModel: ViewModel
+  
+  func makeUIView(context: Context) -> ARView {
+    let arView = ARView(frame: .zero)
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
+    // Load the scene from the "Experience" Reality File
+    let mainSceneAnchor = try! Experience.loadMainScene()
+    
+    let allDisplayActions = mainSceneAnchor.actions.allActions.filter {
+      $0.identifier.hasPrefix("Display")
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    for displayAction in allDisplayActions {
+      displayAction.onAction = { entity in
+        if let entity {
+          viewModel.text = entity.name
+        }
+      }
+    }
     
+    // What to do when SwiftUI triggers this notification
+    viewModel.onScaleCloudNotification = {
+      mainSceneAnchor.notifications.cloudScaleNotification.post()
+    }
+    
+    // Add anchor to the scene
+    arView.scene.anchors.append(mainSceneAnchor)
+    
+    return arView
+  }
+  
+  func updateUIView(_ uiView: ARView, context: Context) {}
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
